dc56995b57b169f12a5c0d6107b644cb299ecfe7
excoffier <julien.excoffier@ecole.ensicaen.fr>
2024-10-12 15:07:36 +0200

    fix: minor fix

--- src/test/java/fr/ensicaen/ecole/archery/model/ArrowTest.java
+++ src/test/java/fr/ensicaen/ecole/archery/model/ArrowTest.java
@@ -21,15 +21,16 @@ class ArrowTest {
     public void test_valid_arrow_creation(){
         Arrow arrow = new Arrow(new Point(0, 1.5),0,0,50);
         Point position = arrow.getPosition(0);
-        assertEquals(0, position.x);
-        assertEquals(0, position.y);
+        assertEquals(0, position.x, 0.1);
+        assertEquals(1.5, position.y, 0.1);
+
         position = arrow.getPosition(arrow.getFinalDistance());
         assertEquals(0, position.x, 0.1);
         assertEquals(0, position.y,0.1);
     }
 
     @Test
-    public void test_invalid_angle_x_high(){
+    public void test_invalid_angle_X_high(){
         try {
             new Arrow(new Point(0, 1.5), Math.PI / 2,0, 10);
             fail("Expected an IllegalArgumentException to be thrown");
@@ -39,7 +40,7 @@ class ArrowTest {
     }
 
     @Test
-    public void test_invalid_angle_x_low() {
+    public void test_invalid_angle_X_low() {
         try {
             new Arrow(new Point(0, 1.5),-Math.PI / 2, 0, 50);
             fail("Expected an IllegalArgumentException to be thrown");
@@ -49,9 +50,9 @@ class ArrowTest {
     }
 
     @Test
-    public void test_invalid_angle_y_high() {
+    public void test_invalid_angle_Y_high() {
         try {
-            new Arrow(new Point(0, 1.5), Math.PI / 2, 0, 50);
+            new Arrow(new Point(0, 1.5), 0, Math.PI / 2, 50);
             fail("Expected an IllegalArgumentException to be thrown");
         } catch (IllegalArgumentException e) {
             assertEquals("Incorrect angleY", e.getMessage());
@@ -59,9 +60,9 @@ class ArrowTest {
     }
 
     @Test
-    public void test_invalid_angle_y_low() {
+    public void test_invalid_angle_Y_low() {
         try {
-            new Arrow(new Point(0, 1.5), -Math.PI / 2, 0, 50);
+            new Arrow(new Point(0, 1.5), 0, -Math.PI / 2, 50);
             fail("Expected an IllegalArgumentException to be thrown");
         } catch (IllegalArgumentException e) {
             assertEquals("Incorrect angleY", e.getMessage());
