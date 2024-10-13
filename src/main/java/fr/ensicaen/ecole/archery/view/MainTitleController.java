e171fd9c30224dd6f88718fa4be9ffb3bddd6624
Lucie CHAUVET <lucie.chauvet@ecole.ensicaen.fr>
2024-10-13 15:00:21 +0200

    feat: début du title

---
+++ src/main/java/fr/ensicaen/ecole/archery/view/MainTitleController.java
@@ -0,0 +1,27 @@
+package fr.ensicaen.ecole.archery.view;
+
+import fr.ensicaen.ecole.archery.presenter.GamePresenter;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+public class MainTitleController {
+
+    @FXML
+    private Pane _targetArea;
+
+    @FXML
+    private AnchorPane _mainArea;
+
+    @FXML
+    private Pane _powerArea;
+
+    @FXML
+    private Label _arrowLabel;
+
+    @FXML
+    private Label _scoreLabel;
+
+}
