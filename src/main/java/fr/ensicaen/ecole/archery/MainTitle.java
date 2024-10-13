afe7a08c053ea04172d3d2c3ce1c9fbd71c59844
Wester <mathis.dubuisson@ecole.ensicaen.fr>
2024-10-13 15:06:55 +0200

    ee

--- src/main/java/fr/ensicaen/ecole/archery/MainTitle.java
+++ src/main/java/fr/ensicaen/ecole/archery/MainTitle.java
@@ -11,32 +11,25 @@ package fr.ensicaen.ecole.archery;
  */
 
 
-import fr.ensicaen.ecole.archery.view.GameController;
+import fr.ensicaen.ecole.archery.view.MainTitleController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Main extends Application {
+public class MainTitle extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(Main.class.getResource("Game.fxml"));
+        FXMLLoader loader = new FXMLLoader(MainTitle.class.getResource("Game.fxml"));
         Scene scene = new Scene(loader.load(), 1280, 720);
 
         primaryStage.setTitle("Archery");
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
-
-        GameController gameController = loader.getController();
-        scene.addEventHandler(MouseEvent.MOUSE_RELEASED, gameController::onMouseReleased);
-        scene.addEventHandler(MouseEvent.MOUSE_PRESSED, gameController::onMousePressed);
-        scene.addEventFilter(MouseEvent.MOUSE_MOVED, gameController::onMouseMoved);
-        scene.addEventFilter(MouseEvent.MOUSE_DRAGGED, gameController::onMouseMoved);
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/fr/ensicaen/ecole/archery/presenter/MainTitlePresenter.java
@@ -0,0 +1,22 @@
+package fr.ensicaen.ecole.archery.presenter;
+
+/*
+ * ENSICAEN
+ * 6 Boulevard Maréchal Juin
+ * F-14050 Caen Cedex
+ *
+ * This file is owned by ENSICAEN students. No portion of this
+ * document may be reproduced, copied or revised without written
+ * permission of the authors.
+ */
+
+import fr.ensicaen.ecole.archery.model.*;
+import fr.ensicaen.ecole.archery.view.*;
+import javafx.animation.Animation;
+import javafx.animation.Timeline;
+import javafx.animation.KeyFrame;
+import javafx.util.Duration;
+
+public class MainTitlePresenter {
+
+}
