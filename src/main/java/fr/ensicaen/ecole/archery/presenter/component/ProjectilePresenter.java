1d6e86f51f52624390dde49cbf96ae68749e2c0c
Wester <mathis.dubuisson@ecole.ensicaen.fr>
2024-10-19 12:11:33 +0200

    fix: typo

--- src/main/java/fr/ensicaen/ecole/archery/presenter/component/ProjectilePresenter.java
+++ src/main/java/fr/ensicaen/ecole/archery/presenter/component/ProjectilePresenter.java
@@ -24,7 +24,7 @@ public class ProjectilePresenter {
 
     private final Projectile _projectile;
     private final ProjectileView _projectileView;
-    private final AdapterTransformationSpace _Adapter_transformationSpace;
+    private final AdapterTransformationSpace _adapterTransformationSpace;
     private final double _scaleDepth;
     private double _depth = 0;
 
@@ -32,7 +32,7 @@ public class ProjectilePresenter {
         _scaleDepth = 0.02 * projectile.getPower();
         _projectile = projectile;
         _projectileView = projectileView;
-        _Adapter_transformationSpace = adapterTransformationSpace;
+        _adapterTransformationSpace = adapterTransformationSpace;
     }
 
     public void updateView() {
@@ -40,12 +40,12 @@ public class ProjectilePresenter {
         moveProjectile();
         Point position = _projectile.computePositionFromDistance(_depth);
         position.z = _depth;
-        Point positionRender = _Adapter_transformationSpace.project3DPointTo2D(position);
-        double renderRadius = _Adapter_transformationSpace.transformRadius(position, radius);
+        Point positionRender = _adapterTransformationSpace.project3DPointTo2D(position);
+        double renderRadius = _adapterTransformationSpace.transformRadius(position, radius);
         Point position1 = _projectile.computePositionFromDistance(_depth);
         Point position2 = _projectile.computePositionFromDistance(_depth + _scaleDepth * _projectile.distanceWhereProjectileStopped());
-        double angle = _Adapter_transformationSpace.computeAngleRotationBetweenTwoPoints(position1, position2);
-        positionRender = _Adapter_transformationSpace.translatePointInCircleOnTopCornerSquare(positionRender, renderRadius, Math.toRadians(angle));
+        double angle = _adapterTransformationSpace.computeAngleRotationBetweenTwoPoints(position1, position2);
+        positionRender = _adapterTransformationSpace.translatePointInCircleOnTopCornerSquare(positionRender, renderRadius, Math.toRadians(angle));
         _projectileView.drawProjectile(positionRender, angle, renderRadius);
 
     }
